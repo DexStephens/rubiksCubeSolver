50909f1528d4faace8f5e816944bbbbf891c4758
DexStephens <dexterfstephens@gmail.com>
2025-01-07 22:40:38 -0700

    Write out directions

--- Cube.py
+++ Cube.py
@@ -20,6 +20,8 @@ class Cube:
         """
         This consists of white cross and white corners
         """
+        # White cross: no specific algorithm
+        # White corners: R' D' R D, repeat until corner is oriented correctly, adjust the position of the corner to align under it's destination and reapply
         pass
 
     
@@ -27,12 +29,17 @@ class Cube:
         """
         This consists of solving the middle layer edges
         """
+        # For edges moving left: U' L' U L U F U' F'
+        # For edges moving right: U R U' R' U' F' U F
         pass
     
     def solveTopLayer(self):
         """
         This consists of yellow cross, yellow corners, and yellow edges
         """
+        # Yellow cross: Line pattern(F R U R' U' F'), L pattern(F U R U' R' F'), applied until a yellow cross forms
+        # Yellow corners: U R U' L' U R' U' L, repeated until all corners are in the correct position
+        # Yellow edges: R' D' R D, position the incorrect corner on the bottom right of the top face, apply repeatedly until the corner is oriented correctly, move next incorrect corner into place and repeat
         pass
 
     def performOperation(self, face: str, clockwise: bool):
